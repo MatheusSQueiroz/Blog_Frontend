7dfe3a445ec1bb4d302379e94f421ae0654be9e2
MatheusSQueiroz <matheussq15@gmail.com>
2023-10-26 13:43:23 -0300

    implementando Sair na Navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,24 +1,38 @@
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+import { AuthContext } from "../../contexts/AuthContext";
 
 function Navbar() {
-    return (
-        <>
-            <div className='w-full bg-indigo-900 text-white
-                flex justify-center py-4'>
-            
-                <div className="container flex justify-between text-lg">
-                    Blog Pessoal
+  const navigate = useNavigate();
+  const { handleLogout } = useContext(AuthContext);
 
-                    <div className='flex gap-4'>
-                        Postagens
-                        Temas
-                        Cadastrar tema
-                        Perfil
-                        Sair
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+  function logout() {
+    handleLogout();
+    alert("Usuário deslogado com sucesso");
+    navigate("/login");
+  }
+  return (
+    <>
+      <div
+        className="w-full bg-indigo-900 text-white
+                flex justify-center py-4"
+      >
+        <div className="container flex justify-between text-lg">
+          <Link to="/home" className="text-2xl font-bold">
+            Blog Pessoal
+          </Link>
+
+          <div className="flex gap-4">
+            Postagens Temas Cadastrar tema Perfil
+            <Link to="" onClick={logout} className="hover:underline">
+              Sair
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
