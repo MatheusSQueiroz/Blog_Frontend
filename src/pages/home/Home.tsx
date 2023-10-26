e0d8d3c1ded36646fb9f25b00dd66a19e8b9b1a2
MatheusSQueiroz <matheussq15@gmail.com>
2023-10-26 13:38:05 -0300

    Atualizando a Home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,7 +3,6 @@ import { AuthContext } from "../../contexts/AuthContext"
 
 function Home() {
     
-    const { nome } = useContext(AuthContext)
     
     return (
         <>
@@ -11,7 +10,7 @@ function Home() {
                 <div className='container grid grid-cols-2 text-white'>
                     <div className="flex flex-col gap-4 items-center justify-center py-4">
                         <h2 className='text-5xl font-bold'>
-                            Seja Bem Vindo, { nome }!
+                            Seja Bem Vindo!
                         </h2>
                         <p className='text-xl'>
                             Expresse aqui seus pensamentos e opniões
